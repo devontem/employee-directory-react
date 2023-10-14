89d1b61d509c1207a1d0e648adeb024ab312c50f
Devonte M <devontem@gmail.com>
2023-10-13 23:30:24 -0400

    Added initial code

--- pages/people/index.tsx
+++ pages/people/index.tsx
@@ -14,16 +14,7 @@ export default function PeoplePage({
 	allPeople,
 	allDepartments,
 }: Props): React.ReactElement {
-	return (
-		<main className="g-grid-container">
-			<h2>People Data</h2>
-			<pre className={style.myData}>{JSON.stringify(allPeople, null, 2)}</pre>
-			<h2>Departments Data</h2>
-			<pre className={style.myData}>
-				{JSON.stringify(allDepartments, null, 2)}
-			</pre>
-		</main>
-	)
+	return <main></main>
 }
 
 export async function getStaticProps(): Promise<GetStaticPropsResult<Props>> {
