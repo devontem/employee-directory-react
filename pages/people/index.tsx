138036d9ca11824ef28a9c527a01b5b7bedb6df8
Devonte M <devontem@gmail.com>
2023-10-13 23:35:56 -0400

    Added JSX markup and filter method

--- pages/people/index.tsx
+++ pages/people/index.tsx
@@ -4,17 +4,50 @@ import { PersonRecord, DepartmentRecord } from 'types'
 import BaseLayout from '../../layouts/base'
 import style from './style.module.css'
 import query from './query.graphql'
+import React, { useState } from 'react'
 
 interface Props {
 	allPeople: PersonRecord[]
 	allDepartments: DepartmentRecord[]
 }
 
+const PersonCard: React.FC<PersonRecord> = ({ name }) => (
+	<div className={style.personCard}>
+		<h3>{name}</h3>
+	</div>
+)
+
 export default function PeoplePage({
 	allPeople,
 	allDepartments,
 }: Props): React.ReactElement {
-	return <main></main>
+	const [searchTerm, setSearchTerm] = useState('')
+
+	const filteredPeople = allPeople.filter((person) =>
+		person.name.toLowerCase().includes(searchTerm.toLowerCase())
+	)
+
+	return (
+		<div>
+			<div className={style['header-container']}>
+				<div className={style.header}>HashiCorp Humans</div>
+				<input
+					type="text"
+					placeholder="Search people by name"
+					value={searchTerm}
+					onChange={(e) => setSearchTerm(e.target.value)}
+					className={style['search-input']}
+				/>
+			</div>
+			<div className={style.container}>
+				<main className={style.peopleList}>
+					{filteredPeople.map((person) => (
+						<PersonCard key={person.name} {...person} />
+					))}
+				</main>
+			</div>
+		</div>
+	)
 }
 
 export async function getStaticProps(): Promise<GetStaticPropsResult<Props>> {
