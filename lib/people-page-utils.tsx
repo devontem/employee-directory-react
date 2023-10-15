9758489a389f65ba9d82ee8d09f36bcc4589443d
Devonte M <devontem@gmail.com>
2023-10-14 20:06:14 -0400

    Added TS Types for  helper method

--- lib/people-page-utils.tsx
+++ lib/people-page-utils.tsx
@@ -4,12 +4,19 @@ export const getRootDepartments = (departments: DepartmentRecord[]) => {
 	return departments.filter((dept) => !dept.parent)
 }
 
+interface FilterArgs {
+	allPeople: PersonRecord[]
+	selectedDepartment?: string | null
+	hideWithoutImage: boolean
+	searchTerm: string
+}
+
 export const getFilteredPeople = ({
 	allPeople,
 	selectedDepartment,
 	hideWithoutImage,
 	searchTerm,
-}) => {
+}: FilterArgs) => {
 	return allPeople.filter(
 		(person) =>
 			person.name.toLowerCase().includes(searchTerm.toLowerCase()) &&
